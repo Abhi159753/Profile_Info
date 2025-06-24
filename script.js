8ea94b0dfe1b2ef41a23aaae6a9e8be9705b1932
Abhijeet Gaikar <138799211+Abhi159753@users.noreply.github.com>
2025-06-24 12:41:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ['<i>Logic is Solution.</i>', 'Computer Student.', 'Full Stack Developer.'],
+    typeSpeed: 65,
+    backSpeed: 40,
+    loop: true
+  });
